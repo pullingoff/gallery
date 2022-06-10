915b8b847c2b03ed6295d40735db4a9cbfd1ad05
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-06-10 19:23:19 +0900

    feat : 헤더 클릭시 리프레쉬

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = () => {
+
+    const refreshPage = () => {
+        window.location.reload();
+      }
+
     return (
-        <StyledHeader>PHOTOTYPE</StyledHeader>
+        <StyledHeader onClick={refreshPage}>
+            PHOTOTYPE
+        </StyledHeader>
     )
 }
 
 const StyledHeader = styled.header`
 font-weight: bold;
 font-size: 3rem;
+padding: 2rem;
+cursor: pointer;
+
 `
 
 export default Header;
